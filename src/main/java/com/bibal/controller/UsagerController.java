7bc66c6b2f9610db49432e0adc51ffb0d0c7da91
idSaid <id.said@live.fr>
2017-03-28 00:27:05 +0100

    pretty template

--- src/main/java/com/bibal/controller/UsagerController.java
+++ src/main/java/com/bibal/controller/UsagerController.java
@@ -3,6 +3,9 @@ package com.bibal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +16,29 @@ import com.bibal.metier.Usager;
 import com.bibal.service.interfaces.UsagerService;
 
 
+@Configuration
 @Controller
+@EnableAutoConfiguration
+@ComponentScan
 public class UsagerController {
 	
 	@Autowired
 	private UsagerService usagerService;
 	
-	@RequestMapping("/index")
-	public String index(){
-		return "index";
-	}
+//	@RequestMapping("/")
+//	public String pageParDefaut(){
+//		return "index";
+//	}
+//	
+//	@RequestMapping("/index")
+//	public String index(){
+//		return "index";
+//	}
+//	
+//	@RequestMapping("/ListeUsagers")
+//	public String ListeUsagers(){
+//		return "ListeUsagers";
+//	}
 
 	@RequestMapping("/addUsager")
 	public String ajouterUsager(@RequestParam("prenom")String prenom, @RequestParam("nom")String nom,
